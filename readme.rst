79298f6fe001487313e45a6965d27cd56ab6a3c9
lior <mail@liorbass.dev>
2022-02-12 16:03:30 +0200

    Updated readme

--- readme.rst
+++ readme.rst
@@ -1,3 +1,13 @@
+
+pip install confpars
+
 This module is built to ease creating and managing configurations.
 
 This module takes a lot of influence from the argparase module.
+
+Features:
+*  Support nested configurations- built with composite pattern.
+*  Support json/yaml/csv and more configurations as input.
+*  Type validation.
+*  Required/optional parameters
+*  Generate an example json file with description and typing.
--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
-setup(name='confpars', version='0.1.0', author='Lior Bass', author_email='mail@liorbass.dev',
-      install_requires=required, scripts=['conf_parser'])
+with open('readme.rst') as f:
+    description=f.read()
+setup(name='confpars', version='0.1.2', author='Lior Bass', author_email='mail@liorbass.dev',
+      install_requires=required, scripts=['conf_parser'], url='https://github.com/liorbass/confpars', description=description)
